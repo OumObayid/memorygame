2b84a28c65ef418c793b373daff1a8f1be54872d
oumobayid <elobayidoumaima@gmail.com>
2021-08-22 00:12:00 +0100

    upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
  //music de fond aau cours de la partie du jeu
- var audioMusicFond = new Audio('audios/creepy.mp3');
- audioMusicFond.play();
+ var monElementAudio = document.getElementById('BgMusic');
+ monElementAudio.volume = 0.1;
  //compteur de secondes
  var seconds = 60; // parametrable
  document.getElementById("timer").textContent = seconds;
